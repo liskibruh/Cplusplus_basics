eaeb067968f25dd0400ed9aff44736e2540f92a7
Owais Tahir <oo_wais@hotmail.com>
2023-10-20 17:39:00 +0500

    Add files via upload

---
+++ arrays_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	int my_array[2][2] = 
+		{
+			{2,4},
+			{5,5}
+		};
+
+
+	cout << my_array[0][1] << endl;
+
+return 0;
+
+}
