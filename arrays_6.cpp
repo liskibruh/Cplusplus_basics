29b7703035506cbd06073807417bd28915076fa9
Owais Tahir <oo_wais@hotmail.com>
2023-10-24 00:37:12 +0500

    Add files via upload

---
+++ arrays_6.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	int some_array[3][2] = 
+	{
+		{2,3},
+		{4,8},
+		{6,8}
+	};
+
+
+	int some_array2[3][2] = 
+	{
+		{4,7},
+		{2,5},
+		{9,3}
+	};
+
+
+	int final_array[3][2]={0};
+
+
+	for(int i=0; i<3; i++) {
+		for(int j=0; j<2; j++) {
+			final_array[i][j]=some_array[i][j]-some_array2[i][j];
+		}
+	}
+
+
+	for(int k=0; k<3; k++) {
+		for(int l=0; l<2; l++) {
+			cout << "Array element " << k << "," << l << ": "<<final_array[k][l] << endl;
+		}
+	}
+
+
+	return 0;
+}
